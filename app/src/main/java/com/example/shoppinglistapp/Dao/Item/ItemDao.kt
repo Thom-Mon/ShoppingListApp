857e8a3f2761272816ddb599b7f7725c0b9b2999
Thom-Geb <t.gebel@zebresel.com>
2022-12-30 14:39:11 +0100

    #2 added RoomDB and Item table

---
+++ app/src/main/java/com/example/shoppinglistapp/Dao/Item/ItemDao.kt
@@ -0,0 +1,46 @@
+package com.example.energy_meter_roomdb
+
+import androidx.room.*
+import com.example.shoppinglistapp.Dao.Item.Item
+import java.util.Locale.Category
+
+
+@Dao
+interface ItemDao {
+
+    @Query("SELECT * FROM item_table")
+    fun getAll(): List<Item>
+
+    @Query("SELECT * FROM item_table WHERE category LIKE :category")
+    suspend fun findByCategory(category: String): List<Item>
+
+    @Query("SELECT * FROM item_table WHERE category LIKE :status")
+    suspend fun findByStatus(status: Int): List<Item>
+
+    // This function is used to determine if a item needs to be created or updated
+    @Query("SELECT * FROM item_table WHERE category LIKE :name Limit 1")
+    suspend fun findByName(name: String): Item
+
+    @Query("SELECT * FROM item_table WHERE id LIKE :id LIMIT 1")
+    suspend fun findById(id: Int): Item
+
+    @Query("SELECT EXISTS (SELECT * FROM item_table WHERE id LIKE :id LIMIT 1)")
+    suspend fun isExistingId(id: Int): Boolean
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("DELETE FROM item_table")
+    suspend fun  deleteAll()
+
+    @Query("UPDATE item_table SET name=:name, category=:category, status=:status WHERE id = :id")
+    suspend fun update(id: Int, name : String , category: String , status: Int)
+
+
+    //get last inserted id
+    @Query("SELECT seq FROM sqlite_sequence WHERE name = :tableName")
+    abstract fun getSequenceNumber(tableName: String): Long?
+}
